c590aef53ca36167d9e88c7b9dc4a43b29e77c5b
Adel <adeljalqadi@gmail.com>
2024-01-02 20:06:01 -0600

    Support export of visual keys, reduce likelihood of outanim file still being open in blender after export, reduced duplicate code

--- io_export_hedgehog_engine_uncompressed_anim.py
+++ io_export_hedgehog_engine_uncompressed_anim.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Hedgehog Engine 2 Uncompressed Animation Export",
     "author": "WistfulHopes, AdelQ",
-    "version": (1, 1, 0),
+    "version": (1, 1, 1),
     "blender": (3, 3, 0),
     "location": "File > Import-Export",
     "description": "A script to export uncompressed animations for Hedgehog Engine 2 games",
@@ -38,7 +38,7 @@ class HedgeEngineAnimationExport(bpy.types.Operator, ExportHelper):
     
     scale_correct: EnumProperty(
         items=[
-            ("accurate", "Accurate", "Make output animation appear exactly as it does in the viewport by correcting bone offests from parent scaling (slower). Ensure that all your bones' scale inheritance mode is set to \"Aligned\" before exporting", 1),
+            ("accurate", "Accurate", "Make output animation appear exactly as it does in the viewport by correcting bone offests from parent scaling (slower). Will set all bones' scale inheritance mode to \"Aligned\" before exporting", 1),
             ("legacy", "Legacy", "Export using the original plugin's method (inaccurate display, but can (potentially) speed up the process of batch retargets if you know what you're doing. Use accurate mode wherever possible", 3),
             ],
         name="Scale Mode",
@@ -97,173 +97,130 @@ class HedgeEngineAnimationExport(bpy.types.Operator, ExportHelper):
         
     def execute(self, context):
         Arm = bpy.context.active_object
+        Scene = bpy.context.scene
         if not Arm:
             raise ValueError("No active object. Please select an armature as your active object.")
-        Scene = bpy.context.scene
+        if Arm.type != 'ARMATURE':
+            raise TypeError(f"Active object \"{Arm.name}\" is not an armature. Please select an armature.")
         objectScale = Arm.scale.copy()
+        CurrentFrame = Scene.frame_current
+        CurrentMode = bpy.context.mode
+        utils_set_mode("POSE")
+        for Bone in Arm.data.bones:
+            Bone.inherit_scale = "ALIGNED"
         
-        if Arm.type == 'ARMATURE':
-            CurFile = open(self.filepath,"wb")
-            
-            CurrentFrame = Scene.frame_current
-            CurrentMode = bpy.context.mode
-            
-            utils_set_mode("POSE")
-            
-            for Bone in Arm.data.bones:
-                Bone.inherit_scale = "ALIGNED"
-            
+        with open(self.filepath,"wb") as CurFile:
             action = Arm.animation_data.action
             
             CurFile.write(struct.pack('<f', float(Scene.frame_end - Scene.frame_start) / Scene.render.fps))
             CurFile.write(struct.pack('<i', Scene.frame_end - Scene.frame_start + 1))
             CurFile.write(struct.pack('<i', len(Arm.pose.bones)))
             
-            
-            # Corrects bone positions as a result of scaling; should be used wherever possible
-            if self.scale_correct == "accurate":
-                for x in range(Scene.frame_end - Scene.frame_start + 1):
-                    Scene.frame_set(Scene.frame_start + x)
-                    
-                    # Set up dictionaries
+            for x in range(Scene.frame_end - Scene.frame_start + 1):
+                Scene.frame_set(Scene.frame_start + x)
+                
+                # Corrects bone positions as a result of scaling; should be used wherever possible
+                if self.scale_correct == "accurate":
                     BoneMats = {}
                     BoneScales = {}
-                    for y in Arm.pose.bones: 
-                        BoneMats.update({y.name:mathutils.Matrix()})
-                        BoneScales.update({y.name:y.scale.copy()})
-                    BoneMatsNoScale = BoneMats.copy()
                     
-                    # Get scaled positions
-                    for y in range(len(Arm.pose.bones)):
-                        Bone = Arm.pose.bones[y]
+                    # Get scaled positions, reset scales
+                    for Bone in Arm.pose.bones:
                         BoneMats.update({Bone.name:Bone.matrix.copy()})
+                        BoneScales.update({Bone.name:Bone.scale.copy()})
                         Bone.scale = mathutils.Vector((1.0,1.0,1.0))
                     
-                    # Apply scaled bone positions to unscaled bones, record new positions
-                    for y in root_bones(Arm):
-                        queue = deque([(y, 0)])
-                        CurrentLevel = 0 
-                        BonesInLevel = 1       
-                        BonesProcessed = 0
-
-                        while queue:
-                            Bone, Level = queue.popleft()
-                            if Level != CurrentLevel: #.............Track level of bone hierarchy, only updating scene once an entire level is cleared instead of for each bone (HUGE performance improvement)
-                                bpy.context.view_layer.update()
-                                CurrentLevel = Level
-                                BonesProcessed = 1
-                                BonesInLevel = len(queue) + 1
-                            else:
-                                BonesProcessed += 1
-
-                            Bone.matrix.translation = BoneMats[Bone.name].translation 
-                            
-                            for z in Bone.children:
-                                queue.append((z, Level + 1))
-                            
-                            if BonesProcessed == BonesInLevel:
-                                bpy.context.view_layer.update() 
+                    # Updated scaled positions to unscaled bones
+                    queue_transforms(Arm, BoneMats)
                                 
                     # Caclulate and write transforms to file
-                    for y in range(len(Arm.pose.bones)):
-                        Bone = Arm.pose.bones[y]
+                    for Bone in Arm.pose.bones:
                         if Bone.parent:
                             ParentMat = Bone.parent.matrix.copy()
+                        elif self.use_yx_orientation:
+                            ParentMat = mathutils.Matrix() @ mathutils.Matrix.Rotation(math.radians(-90.0), 4, 'X') @ mathutils.Matrix.Rotation(math.radians(-90.0), 4, 'Z')
                         else:
-                            if self.use_yx_orientation:
-                                ParentMat = mathutils.Matrix() @ mathutils.Matrix.Rotation(math.radians(-90.0), 4, 'X') @ mathutils.Matrix.Rotation(math.radians(-90.0), 4, 'Z')
-                            else:
-                                ParentMat = mathutils.Matrix() 
-
+                            ParentMat = mathutils.Matrix() 
                         tmpQuat = (ParentMat.inverted() @ Bone.matrix.copy()).to_quaternion()
                         tmpPos = (ParentMat.inverted() @ mathutils.Matrix.Translation(Bone.matrix.translation)).translation
-                        if self.use_scale_factor == "manual":
-                            tmpPos *= self.scale_factor_value
-                        elif self.use_scale_factor == "object":
-                            tmpPos = mathutils.Vector((tmpPos[0]*objectScale[0],tmpPos[1]*objectScale[1],tmpPos[2]*objectScale[2]))
                         tmpScl = BoneScales[Bone.name]
                         
-                        if self.use_yx_orientation:
-                            CurFile.write(struct.pack('<ffff', tmpQuat[2],tmpQuat[3],tmpQuat[1],tmpQuat[0])) 
-                            CurFile.write(struct.pack('<fff', tmpPos[1],tmpPos[2],tmpPos[0])) 
-                            CurFile.write(struct.pack('<f', 0.0)) 
-                            CurFile.write(struct.pack('<fff', tmpScl[1],tmpScl[2],tmpScl[0])) 
-                            CurFile.write(struct.pack('<f', 1.0)) 
-                        else:
-                            CurFile.write(struct.pack('<ffff', tmpQuat[1],tmpQuat[2],tmpQuat[3],tmpQuat[0])) #......Rotations
-                            CurFile.write(struct.pack('<fff', tmpPos[0],tmpPos[1],tmpPos[2])) #.....................Locations
-                            CurFile.write(struct.pack('<f', 0.0)) #.................................................Unknown Float, has value but unsure of purpose
-                            CurFile.write(struct.pack('<fff', tmpScl[0],tmpScl[1],tmpScl[2])) #.....................Scales
-                            CurFile.write(struct.pack('<f', 1.0)) #.................................................Seems to always be 1.0
-                        
-            # Method used in original plugin. Not recommended if scaling is used
-            elif self.scale_correct == "legacy":
-                for x in range(Scene.frame_end - Scene.frame_start + 1):
-                    Scene.frame_set(Scene.frame_start + x)
-                    for y in range(len(Arm.pose.bones)):
-                        Bone = Arm.pose.bones[y]
+                        anim_export(CurFile, Arm, tmpPos, tmpQuat, tmpScl, self)
+                            
+                    # Restore old transforms in case of missing keyframes  
+                    for Bone in Arm.pose.bones:
+                        Bone.scale = BoneScales[Bone.name]
+                    queue_transforms(Arm, BoneMats)
+        
+                # Original plug-in method, only marginally faster and does not treat scales properly
+                elif self.scale_correct == "legacy":
+                    for Bone in Arm.pose.bones:
                         if Bone.parent:
                             Mat = Bone.parent.matrix.inverted() @ Arm.convert_space(pose_bone=Bone,matrix = Bone.matrix_basis,from_space='LOCAL',to_space='POSE')
+                        elif self.use_yx_orientation:
+                            Mat = Arm.convert_space(pose_bone=Bone,matrix = Bone.matrix_basis,from_space='LOCAL',to_space='POSE') @ mathutils.Matrix.Rotation(math.radians(90.0), 4, 'Z') @ mathutils.Matrix.Rotation(math.radians(90.0), 4, 'X')
                         else:
-                            if self.use_yx_orientation:
-                                Mat = Arm.convert_space(pose_bone=Bone,matrix = Bone.matrix_basis,from_space='LOCAL',to_space='POSE') @ mathutils.Matrix.Rotation(math.radians(90.0), 4, 'Z') @ mathutils.Matrix.Rotation(math.radians(90.0), 4, 'X')
-                            else:
-                                Mat = Arm.convert_space(pose_bone=Bone,matrix = Bone.matrix_basis,from_space='LOCAL',to_space='POSE')
-                        
-                        
+                            Mat = Arm.convert_space(pose_bone=Bone,matrix = Bone.matrix_basis,from_space='LOCAL',to_space='POSE')
                         tmpQuat = Mat.to_quaternion()
                         tmpPos = Mat.translation
-                        if self.use_scale_factor == "manual":
-                            tmpPos *= self.scale_factor_value
-                        elif self.use_scale_factor == "object":
-                            tmpPos = mathutils.Vector((tmpPos[0]*objectScale[0],tmpPos[1]*objectScale[1],tmpPos[2]*objectScale[2]))
                         tmpScl = Bone.scale
                         
-                        if self.use_yx_orientation:
-                            CurFile.write(struct.pack('<f', tmpQuat[2]))
-                            CurFile.write(struct.pack('<f', tmpQuat[3]))
-                            CurFile.write(struct.pack('<f', tmpQuat[1]))
-                            CurFile.write(struct.pack('<f', tmpQuat[0]))
-                            
-                            CurFile.write(struct.pack('<f', tmpPos[1]))
-                            CurFile.write(struct.pack('<f', tmpPos[2]))
-                            CurFile.write(struct.pack('<f', tmpPos[0]))
-                            CurFile.write(struct.pack('<f', 0.0))
-                            
-                            CurFile.write(struct.pack('<f', tmpScl[1]))
-                            CurFile.write(struct.pack('<f', tmpScl[2]))
-                            CurFile.write(struct.pack('<f', tmpScl[0]))
-                            CurFile.write(struct.pack('<f', 0.0))
-                        else:
-                            CurFile.write(struct.pack('<f', tmpQuat[1]))
-                            CurFile.write(struct.pack('<f', tmpQuat[2]))
-                            CurFile.write(struct.pack('<f', tmpQuat[3]))
-                            CurFile.write(struct.pack('<f', tmpQuat[0]))
-                            
-                            CurFile.write(struct.pack('<f', tmpPos[0]))
-                            CurFile.write(struct.pack('<f', tmpPos[1]))
-                            CurFile.write(struct.pack('<f', tmpPos[2]))
-                            CurFile.write(struct.pack('<f', 0.0))
-                            
-                            CurFile.write(struct.pack('<f', tmpScl[0]))
-                            CurFile.write(struct.pack('<f', tmpScl[1]))
-                            CurFile.write(struct.pack('<f', tmpScl[2]))
-                            CurFile.write(struct.pack('<f', 0.0))
+                        anim_export(CurFile, Arm, tmpPos, tmpQuat, tmpScl, self)
             
-            Scene.frame_current = CurrentFrame
-            utils_set_mode(CurrentMode)             
-            CurFile.close()
-            del CurFile
-        else:
-                raise TypeError(f"Active object \"{Arm.name}\" is not an armature. Please select an armature.")
+                else:
+                    raise TypeError("None or invalid scale correction method.")
+            
+        Scene.frame_current = CurrentFrame
+        utils_set_mode(CurrentMode)             
+        
         return {'FINISHED'}
     
 def utils_set_mode(mode):
     if bpy.ops.object.mode_set.poll():
         bpy.ops.object.mode_set(mode=mode, toggle=False)
-        
-def root_bones(Arm):
-    return [Bone for Bone in Arm.pose.bones if not Bone.parent]
+
+def queue_transforms(Arm, BoneMats):
+    root_bones = [Bone for Bone in Arm.pose.bones if not Bone.parent]
+    for y in root_bones:
+        queue = deque([(y, 0)])
+        CurrentLevel = 0 
+        BonesInLevel = 1       
+        BonesProcessed = 0
+        while queue:
+            Bone, Level = queue.popleft()
+            if Level != CurrentLevel:
+                CurrentLevel = Level
+                BonesProcessed = 1
+                BonesInLevel = len(queue) + 1
+            else:
+                BonesProcessed += 1
+
+            Bone.matrix.translation = BoneMats[Bone.name].translation 
+            
+            for ChildBone in Bone.children:
+                queue.append((ChildBone, Level + 1))
+            
+            if BonesProcessed == BonesInLevel:
+                bpy.context.view_layer.update() 
+
+def anim_export(CurFile, Arm, tmpPos, tmpQuat, tmpScl, ClassSelf):
+    if ClassSelf.use_scale_factor == "manual":
+        tmpPos *= ClassSelf.scale_factor_value
+    elif ClassSelf.use_scale_factor == "object":
+        tmpPos = mathutils.Vector((tmpPos[0]*objectScale[0],tmpPos[1]*objectScale[1],tmpPos[2]*objectScale[2]))
+
+    if ClassSelf.use_yx_orientation:
+        CurFile.write(struct.pack('<ffff', tmpQuat[2],tmpQuat[3],tmpQuat[1],tmpQuat[0])) 
+        CurFile.write(struct.pack('<fff', tmpPos[1],tmpPos[2],tmpPos[0])) 
+        CurFile.write(struct.pack('<f', 0.0)) 
+        CurFile.write(struct.pack('<fff', tmpScl[1],tmpScl[2],tmpScl[0])) 
+        CurFile.write(struct.pack('<f', 1.0)) 
+    else:
+        CurFile.write(struct.pack('<ffff', tmpQuat[1],tmpQuat[2],tmpQuat[3],tmpQuat[0]))
+        CurFile.write(struct.pack('<fff', tmpPos[0],tmpPos[1],tmpPos[2]))
+        CurFile.write(struct.pack('<f', 0.0))
+        CurFile.write(struct.pack('<fff', tmpScl[0],tmpScl[1],tmpScl[2]))
+        CurFile.write(struct.pack('<f', 1.0))
 
 def menu_func_export(self, context):
     self.layout.operator(HedgeEngineAnimationExport.bl_idname, text="HE2 Uncompressed Animation (.outanim)")
